5c8c84d2784dc54bdf39684867aa431cbb63ad4d
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-05-09 07:39:26 -0400

    fix issue with hover_data

--- src/figure.py
+++ src/figure.py
@@ -373,7 +373,7 @@ def generate_figure(data, selected_data, subset_vars, subsets, form_inputs, *arg
     px_kwargs["hover_data"] = (
         px_kwargs["hover_data"].split(",")
         if isinstance(px_kwargs.get("hover_data"), str)
-        else px_kwargs["hover_data"]
+        else px_kwargs.get("hover_data")
     )
     if px_kwargs.get("x") is None or px_kwargs.get("y") is None:
         logger.debug("No x or y axis given: px_kwargs=%s", px_kwargs)
