11a746e3acac4b4bf0d0ada73f5e0a833d4693c5
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-11-24 06:53:00 -0500

    missing empty output to welcome up

--- hakai_qc_app/welcome.py
+++ hakai_qc_app/welcome.py
@@ -200,7 +200,7 @@ def get_datatype_from_pathname(pathname, welcome_is_open):
 )
 def show_welcome_page(path, search, valid_credentials, n_clicks):
     if not valid_credentials:
-        return False
+        return False, False
     data_type = path.split("/")[1]
     logger.debug("show welcome for data_type={} in path={}", data_type, path)
     no_search = len(search) < 10
